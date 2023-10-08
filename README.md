694a5e74cf0454ef09c88d607f121268c15efdf3
DeniskaRediska21 <denisgontar99@gmail.com>
2023-10-08 14:32:42 +0300

    Filled readme, fixed bug with different fontsizes on windown and ubuntu

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# Image-to-ASCII
+# img_to_ASCII
+
+Python function that turns any image to ASCII-art.
+
+## Function defenition
+
+img_to_ASCII(PATH:str, chars = [' ', '.','+','=', '#'], spacing = 10, scale = 0.1,background = (255,255,255),textcolor = (0,0,0), verbose = True, original_resolution = False, flip = True, sys_font_size = (6,6))
+
+### Inputs
+
++ PATH (Str)- path to image that you want to convert
++ chars (list of Char) - ASCII characters that will represent colors
++ spacing (Int) - spasing berween chars in the final image, when in 'grayscale mode'
++ scale (Float) - final image scale (original_img_scale/final_img_scale) for HD images it's best to use 0.1 - 0.2
++ background (RGB triplet) - color of the final image's background 
++ textcolor (RGB triplet/ 'original') - color of the final image's text. If provided with RGB triplet, works in grayscale mode,  
++ if provided with keyword 'original' works in full color mode, preserving original image colors
++ verbose (True/False) - if True shows final image on screen
++ original_resolution (True/False)- if True final img will be in the same resolution as input image
++ flip (True/False)- if True flips the chars array
++ sys_font_size (touple of 2 Floats) - needed because in different systems default system font size differs (in windows (6,6), in ubuntu (13,6))i
+ 
+### Outputs
+
++ PIL Image - containing final image
++ np.array of str32 - containing characters of final image
+
+## Gallery
+
+### Original colors 0.1 scale
+
+'''
+ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ','.','+','#','@'], spacing = 8, scale = 0.1,background = (255,255,255),textcolor = 'original', verbose = False, original_resolution = False, flip = True,sys_font_size = (13,6))
+ascii_img.save("Results/original_colors_scale_0_1.jpg")
+'''
+
+[Original colors 0.1 scale]<Results/original_colors_scale_0_1.jpg>
+
+_________
+
+### Grayscale 0.1 scale
+
+'''
+ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ', '.', '-','+','#', '@'], spacing = 9, scale = 0.1,background = (255,255,255),textcolor = (0,0,0), verbose = True, original_resolution = False, flip = True, sys_font_size = (13,6))
+ascii_img.save("Results/grayscale_scale_0_1.jpg")
+'''
+
+[Grayscale 0.1 scale]<Results/grayscale_scale_0_1.jpg>
+
+_________
+
+### Original colors 0.2 scale
+
+'''
+ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ','.','+','#','@'], spacing = 8, scale = 0.2,background = (255,255,255),textcolor = 'original', verbose = False, original_resolution = False, flip = True,sys_font_size = (13,6))
+ascii_img.save("Results/original_colors_scale_0_2.jpg")
+'''
+
+[Original colors 0.2 scale]<Results/original_colors_scale_0_2.jpg>
+
+_________
+
+### Grayscale 0.2 scale
+
+'''
+ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ', '.', '-','+','#', '@'], spacing = 9, scale = 0.2,background = (255,255,255),textcolor = (0,0,0), verbose = True, original_resolution = False, flip = True, sys_font_size = (13,6))
+ascii_img.save("Results/grayscale_scale_0_2.jpg")
+'''
+
+[Grayscale 0.1 scale]<Results/grayscale_scale_0_2.jpg>
+
+_________
+
+### Original colors 0.2 scale, black background
+
+'''
+ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ','.','+','#','@'], spacing = 8, scale = 0.2,background = (0,0,0),textcolor = 'original', verbose = False, original_resolution = False, flip = True,sys_font_size = (13,6))
+ascii_img.save("Results/original_colors_scale_0_2_blackBG.jpg")
+'''
+
+[Original colors 0.2 scale black BG]<Results/original_colors_scale_0_2_blackBG.jpg>
+
+_________
---
+++ img_2_ascii.py
@@ -0,0 +1,93 @@
+
+from PIL import Image, ImageDraw
+import numpy as np
+import re
+
+def img_to_ASCII(PATH:str, chars = [' ', '.','+','=', '#'], spacing = 10, scale = 0.1,background = (255,255,255),textcolor = (0,0,0), verbose = True, original_resolution = False, flip = True, sys_font_size = (6,6)): 
+    # converts image to provided chars and outputs them as PIL image
+    
+    # flip char array if needed
+    if flip:
+        chars = np.flip(chars)
+    
+    chars = np.array(chars)
+    img = Image.open(PATH)
+    h,l = img.size
+    img = img.resize((np.array((h,l))*scale).astype(np.uint64))
+    img = np.array(img) 
+    
+    # convert colors to 0 to 1 grayscale
+    # img_g = np.mean(img, axis=2)
+    img_g = np.mean(img, axis=2)/255
+    
+    # convert colors to indexes in chars array
+    # coords = (img_g//(255/len(chars))).astype(np.uint64)
+    coords = (img_g * (len(chars) - 1) + 0.5).astype(np.uint64)
+    
+    #convert index array to str array made from chars
+    coords = chars[coords]
+    
+
+    
+    
+    # needed bc. ImageDraw.text doesn't work with np.array(str)
+    if textcolor == 'original':    
+        L,H = spacing*np.array((coords.shape))
+        n_img = Image.new('RGB', (H,L),background)
+        d = ImageDraw.Draw(n_img)
+        
+        # for every pixel
+        for row in range(coords.shape[0]):    
+            for column in range(coords.shape[1]):
+                # draw text
+                d.text((column*spacing, row*spacing),str(coords[row,column]),fill = tuple(img[row,column,:]))
+    else:
+        # doublle each column to match char width
+        new_coords = np.empty((coords.shape[0],2*coords.shape[1])).astype(str)
+        new_coords[:,0::2] = coords
+        new_coords[:,1::2] = coords
+        coords = new_coords
+        
+        H,L = np.array((coords.shape))*sys_font_size
+        n_img = Image.new('RGB', (L,H),background)
+        d = ImageDraw.Draw(n_img)
+        
+        # conversion from array of str to big str
+        N=coords.astype('|S1').tobytes().decode('UTF8)')
+        # insert newline chars every image width pixels
+        N= re.sub("(.{"+f"{coords.shape[1]}"+"})", "\\1\n", N, 0, re.DOTALL)
+        
+        # draw text
+        d.text((0,0),N,fill = (0,0,0), spacing = 2.5)
+        # for row in range(coords.shape[0]):    
+        #     for column in range(coords.shape[1]):
+        #         d.text((column*spacing, row*spacing),str(coords[row,column]),fill = textcolor)
+
+    
+    
+    # resize image to original resolution if needed
+    if original_resolution:    
+        n_img = n_img.resize((h,l))
+
+    # show on screen if needed
+    if verbose:    
+        n_img.show()
+
+    return n_img, coords
+
+# ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ','.','+','#','@'], spacing = 8, scale = 0.1,background = (255,255,255),textcolor = 'original', verbose = False, original_resolution = False, flip = True,sys_font_size = (13,6))
+# ascii_img.save("Results/original_colors_scale_0_1.jpg")
+# 
+# ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ', '.', '-','+','#', '@'], spacing = 9, scale = 0.1,background = (255,255,255),textcolor = (0,0,0), verbose = True, original_resolution = False, flip = True, sys_font_size = (13,6))
+# ascii_img.save("Results/grayscale_scale_0_1.jpg")
+# 
+# ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ','.','+','#','@'], spacing = 8, scale = 0.2,background = (255,255,255),textcolor = 'original', verbose = False, original_resolution = False, flip = True,sys_font_size = (13,6))
+# ascii_img.save("Results/original_colors_scale_0_2.jpg")
+# 
+# ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ', '.', '-','+','#', '@'], spacing = 9, scale = 0.2,background = (255,255,255),textcolor = (0,0,0), verbose = True, original_resolution = False, flip = True, sys_font_size = (13,6))
+# ascii_img.save("Results/grayscale_scale_0_2.jpg")
+# 
+# ascii_img, chars = img_to_ASCII('Data/Penguins.jpg', chars = [' ','.','+','#','@'], spacing = 8, scale = 0.2,background = (0,0,0),textcolor = 'original', verbose = False, original_resolution = False, flip = True,sys_font_size = (13,6))
+# ascii_img.save("Results/original_colors_scale_0_2_blackBG.jpg")
+
+
